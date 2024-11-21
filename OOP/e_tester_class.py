9018115d024e9aa4158465ed9f50a6dc6a666024
SAYAN-DEV-NATH <sayandevnath01@gmail.com>
2024-11-21 21:52:13 +0600

    update

--- OOP/e_tester_class.py
+++ OOP/e_tester_class.py
@@ -1,5 +1,8 @@
-import e_book_class
+import e_book_class  # import class_name/file_name
 
 book1 = e_book_class.Book("Opekkha", "Humayun Ahmed")
 book1.set_price(250)
 book1.details()
+
+
+# from folder_name import class_name/file_name
