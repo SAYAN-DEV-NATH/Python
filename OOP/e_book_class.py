d30b77ea4cdbb467ca1d747c2a8ed1bef2106963
SAYAN-DEV-NATH <sayandevnath01@gmail.com>
2024-11-21 14:56:54 +0600

    update

--- OOP/e_book_class.py
+++ OOP/e_book_class.py
@@ -22,7 +22,4 @@ class Book:
         )
 
 
-book1 = Book("Opekkha", "Humayun Ahmed")
-book1.details()
-book1.set_price(250)
-book1.details()
+# design class
---
+++ OOP/e_tester_class.py
@@ -0,0 +1,5 @@
+import e_book_class
+
+book1 = e_book_class.Book("Opekkha", "Humayun Ahmed")
+book1.set_price(250)
+book1.details()
