e5d14e6f1536fa038db802a8f9f2e8640a300f46
SAYAN-DEV-NATH <sayandevnath01@gmail.com>
2024-11-24 23:17:41 +0600

    update

---
+++ OOP/n_student.py
@@ -0,0 +1,17 @@
+class Student:
+    count = 0  # static or class variable
+
+    def __init__(self, name, id) -> None:  # instance method
+        self.name = name  # instance variable
+        self.id = id
+        Student.count += 1
+
+    def details(self):
+        print(f"Name: {self.name}, ID: {self.id}, Student count: {self.count}")
+
+
+s1 = Student("Bob", 11)
+s2 = Student("Carol", 22)
+s3 = Student("Mike", 33)
+
+s1.details()
